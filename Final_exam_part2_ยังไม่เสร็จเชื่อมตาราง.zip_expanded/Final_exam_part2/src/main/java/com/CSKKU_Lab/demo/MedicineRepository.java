b8a7e5401a9c0088a05e115d7a924ed8a848739a
Chonsawat <chonsawat.nakanam@kkumail.com>
2021-11-15 16:18:34 +0700

    Update

--- Final_exam_part2_ยังไม่เสร็จเชื่อมตาราง.zip_expanded/Final_exam_part2/src/main/java/com/CSKKU_Lab/demo/MedicineRepository.java
+++
@@ -1,8 +0,0 @@
-package com.CSKKU_Lab.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface MedicineRepository extends JpaRepository<Medicine, Integer> {
-	Medicine findByName(String name);
-}
--- Final_exam_part2_ยังไม่เสร็จเชื่อมตาราง.zip_expanded/Final_exam_part2/src/main/java/com/CSKKU_Lab/demo/TreatmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.CSKKU_Lab.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface TreatmentRepository extends JpaRepository<Treatment, Integer> {
-	Treatment findByName(String name);
-}
